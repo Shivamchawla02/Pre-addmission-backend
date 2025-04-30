2087a3f4e3a1eada9a08bb313a721d9eb4d7a4d2
Shivam <shivamchawla678@gmail.com>
2025-04-30 14:01:40 +0530

    Added .js in index.js imports

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import preAdmissionRoutes from './routes/preAdmissionRoutes';
-import PreAdmission from './models/PreAdmission';
+import preAdmissionRoutes from './routes/preAdmissionRoutes.js';
+import PreAdmission from './models/PreAdmission.js';
 
 const app = express();
 app.use(cors());
